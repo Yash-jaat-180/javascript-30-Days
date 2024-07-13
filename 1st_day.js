e2ab97c1edb9548c69c3e68c14bb26df42cbd066
Yash <yashsaharan180@gmiail.com>
2024-07-13 15:51:52 +0530

    complete first day by understanding basics of datatypes

---
+++ 1st_day.js
@@ -0,0 +1,33 @@
+// Activity one
+var a = 6;
+console.log(a);
+let b = "yash";
+console.log(b);
+
+// Activity Two
+
+const flag = true;
+console.log(flag);
+
+// Activity Three
+
+let obj = {"name": "yash", age: 18};
+let arr = [1,3,4,5,7];
+
+console.log(typeof(a), a);
+console.log(typeof(b), b);
+console.log(typeof(flag), flag);
+console.log(typeof(obj), obj);
+console.log(typeof(arr), arr);
+
+
+// Activity Four
+
+let number = 5;
+console.log(number);
+number = "yash";
+console.log(number);
+
+// Activity Five
+const cantChange = 5;
+console.log(cantChange);
