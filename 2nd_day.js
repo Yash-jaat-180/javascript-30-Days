be99f5babe67e21c5f4afa36626b4f0fd0cea760
Yash <yashsaharan180@gmiail.com>
2024-07-14 13:37:17 +0530

    completed 2nd day understand the operators

---
+++ 2nd_day.js
@@ -0,0 +1,32 @@
+// Activity 1
+let a = 10;
+let b = 6;
+console.log(a + b);
+console.log(a - b);
+console.log(a* b);
+console.log(a / b);
+console.log(a % b);
+
+// Activity 2
+a += 2;
+b -= 2;
+console.log(a);
+console.log(b);
+
+// Activity 3
+let flag = a > b;
+console.log(flag);
+flag = a === b;
+console.log(flag);
+
+
+// Activity 4
+flag = a < b && a > b;
+console.log(flag);
+flag = a < b || a > b;
+console.log(flag);
+console.log(!a);
+
+// Activity 5
+a >= 0 ? console.log("Positive") : console.log("Negative");
+
